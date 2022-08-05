0641ee931ca3810594f7c4d78470c5591a109e26
KrzysztofHum <k.humienny1996@protonmail.com>
2022-08-05 15:04:12 +0200

    inc all types

--- backend/controllers/viewStatsController.js
+++ backend/controllers/viewStatsController.js
@@ -39,3 +39,90 @@ export const incPostViews = expressAsyncHandler(async (req, res) => {
     res.status(200).json({ postStats });
   }
 });
+export const incPostStats = expressAsyncHandler(async (req, res) => {
+  if (req.params.type == "views") {
+    const postStats = await postStatsRespository
+      .search()
+      .where("id")
+      .equal(req.params.id)
+      .return.all();
+    if (postStats.length <= 0) {
+      await postStatsRespository.createIndex();
+      const newStats = await postStatsRespository.createAndSave({
+        id: req.body.id,
+        views: 1,
+        uniqueViews: 0,
+        phoneClicks: 0,
+        favorites: 0,
+      });
+      res.status(200).json({ newStats });
+    } else {
+      postStats[0].views++;
+      await postStatsRespository.save(postStats[0]);
+      res.status(200).json({ postStats });
+    }
+  } else if (req.params.type === "uniqueViews") {
+    const postStats = await postStatsRespository
+      .search()
+      .where("id")
+      .equal(req.params.id)
+      .return.all();
+    if (postStats.length <= 0) {
+      await postStatsRespository.createIndex();
+      const newStats = await postStatsRespository.createAndSave({
+        id: req.body.id,
+        views: 0,
+        uniqueViews: 1,
+        phoneClicks: 0,
+        favorites: 0,
+      });
+      res.status(200).json({ newStats });
+    } else {
+      postStats[0].uniqueViews++;
+      await postStatsRespository.save(postStats[0]);
+      res.status(200).json({ postStats });
+    }
+  } else if (req.params.type === "phoneClicks") {
+    const postStats = await postStatsRespository
+      .search()
+      .where("id")
+      .equal(req.params.id)
+      .return.all();
+    if (postStats.length <= 0) {
+      await postStatsRespository.createIndex();
+      const newStats = await postStatsRespository.createAndSave({
+        id: req.body.id,
+        views: 0,
+        uniqueViews: 0,
+        phoneClicks: 1,
+        favorites: 0,
+      });
+      res.status(200).json({ newStats });
+    } else {
+      postStats[0].phoneClicks++;
+      await postStatsRespository.save(postStats[0]);
+      res.status(200).json({ postStats });
+    }
+  } else if (req.params.type === "favorites") {
+    const postStats = await postStatsRespository
+      .search()
+      .where("id")
+      .equal(req.params.id)
+      .return.all();
+    if (postStats.length <= 0) {
+      await postStatsRespository.createIndex();
+      const newStats = await postStatsRespository.createAndSave({
+        id: req.body.id,
+        views: 0,
+        uniqueViews: 0,
+        phoneClicks: 0,
+        favorites: 1,
+      });
+      res.status(200).json({ newStats });
+    } else {
+      postStats[0].favorites++;
+      await postStatsRespository.save(postStats[0]);
+      res.status(200).json({ postStats });
+    }
+  }
+});
